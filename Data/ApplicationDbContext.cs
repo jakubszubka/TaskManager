4760cb8988e2ca9edf9895ded8004d18461e890c
Jakub Szubka <jakubszubkawork@gmail.com>
2026-02-05 15:07:32 +0100

    Add EF Core setup with TodoTask model and DbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using TaskManager.Models;  // Make sure namespace matches your project
+
+namespace TaskManager.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<TodoTask> Tasks => Set<TodoTask>();
+    }
+}
---
+++ Models/TodoTask.cs
@@ -0,0 +1,18 @@
+﻿namespace TaskManager.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class TodoTask
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DueDate { get; set; }
+    }
+}
