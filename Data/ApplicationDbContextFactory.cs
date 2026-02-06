2e99cf3e844b98a5c6a7ec3e102f15c0aad2eca3
Jakub Szubka <jakubszubkawork@gmail.com>
2026-02-06 11:58:54 +0100

    Configure EF Core, DI, and database setup

---
+++ Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TaskManager.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            // Use the same connection string you put in Program.cs
+            optionsBuilder.UseSqlite("Data Source=tasks.db");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
