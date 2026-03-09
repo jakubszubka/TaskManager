71c37e4371d4b0ed8aa1ba85eaf26416cc8ad121
Jakub Szubka <jakubszubkawork@gmail.com>
2026-03-09 16:19:40 +0100

    Add form validation and error display

--- Models/TodoTask.cs
+++ Models/TodoTask.cs
@@ -5,7 +5,8 @@
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100, ErrorMessage = "Title can't exceed 100 characters")]
         public string Title { get; set; } = string.Empty;
 
         public string? Description { get; set; }
